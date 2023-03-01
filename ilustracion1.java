f625bcabe00c4f38a5f938a66d36241ab1db6d2f
DenisDenislavov <120368798+DenisDenislavov@users.noreply.github.com>
2023-03-01 09:47:20 +0100

    Update ilustracion1.java

--- ilustracion1.java
+++ ilustracion1.java
@@ -1,12 +1,10 @@
 public static boolean esPrimo(int n){
     int i=2;
-    if(n<1){
-        return false;
-    }
-    while(i<n) {
-        if (n%i==0){
+    while(i<n){
+        if(n%i==0)
             return false;
-        }
         i++;
     }
     return true;
+}
+
